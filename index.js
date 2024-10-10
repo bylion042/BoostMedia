69629dde803ea32d5e0aca4439a19e029a640926
Bby Lion <codelab042@gmail.com>
2024-10-10 16:37:53 +0100

    vercel

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-if (process.env.NODE_ENV != 'production') {
-
+if (process.env.NODE_ENV !== 'production') {
     const dotenv = require('dotenv');
     dotenv.config();
 }
+
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const authRoutes = require('./routes/auth');
-
 
 const app = express();
 
@@ -21,16 +20,14 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(session({
-    secret: 'your_secret_key',
+    secret: process.env.SESSION_SECRET || 'fallback_secret_key', // Use an environment variable for the secret
     resave: false,
     saveUninitialized: false
 }));
 
-
 app.get('/', (req, res) => {
     res.render('index'); // Render the index.ejs file
 });
-
 
 // Routes for authentication
 app.use('/', authRoutes);
